34527fa9f0d7801b9e76222760b997e262548eb3
matan solom <matan.s.dev@gmail.com>
2023-12-14 22:01:43 +0200

    Add bodyParser

--- myApp.js
+++ myApp.js
@@ -1,43 +1,60 @@
 let express = require('express');
 let app = express();
 require('dotenv').config();
+var bodyParser =require('body-parser');
 
 console.log("server restarted");  //#1//
 
-app.use('/public',express.static(__dirname + '/public'))  //#4//
 
-//#7
-// app.use((req, res,next)=>{
-//     console.log(req.method + " " +req.path + " "+"-"+" " + req.ip)
-//     next();
-// })
+
+app.use('/public',express.static(__dirname + '/public'));  //#4//
+
+// #7
+app.use((req, res,next)=>{
+    console.log(req.method + " " +req.path + " "+"-"+" " + req.ip)
+    next();
+});
+app.use((req, res, next)=>{
+    bodyParser.urlencoded({extended: false});
+    console.log(bodyParser);
+    next();
+});
 
 app.get("/",function(req, res) {
- res.sendFile(__dirname + '/views/index.html' )
+ res.sendFile(__dirname + '/views/index.html' );
  
-})                                         //#2+3//
+});  //#2+3//
 
 // #5
 app.get("/json",(req, res)=>{
     //#6
  if(process.env.MESSAGE_STYLE=="uppercase"){
-  res.json({"message":"HELLO JSON"})
+  res.json({"message":"HELLO JSON"});
  }else{
-  res.json({"message":"Hello json"})
- }
-})
+  res.json({"message":"Hello json"});
+ };
+});
 
 const getCurrentTime=()=>{
     return new Date().toString();
-}
+};
 app.get("/now",(req, res, next)=>{
     req.time=getCurrentTime();
     next();
 },(req, res)=>{
-    res.json({"time":req.time})
-})
+    res.json({"time":req.time});
+});
+app.get("/:word/echo",(req, res)=>{
+    res.json({ "echo": req.params.word });
+});
 
-
+app.get("/name",(req, res)=>{
+    res.json({"name":req.query.first+ " " +req.query.last})
+    console.log(req.query);
+});
+// .get((req, res)=>{
+//     res.json({"name": 'firstname lsetname'})
+// }).post()
 
 
 
