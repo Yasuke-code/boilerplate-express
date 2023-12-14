c424b0109ff0931a233aee07d74ab99f220dd247
matan solom <141280462+Yasuke-code@users.noreply.github.com>
2023-12-14 04:39:58 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,9 +1,14 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
+
+// console.log("Hello World");  //#1//
+
+app.use('/public',express.static(__dirname + '/public'))                   //#4//
+
 app.get("/",function(req, res) {
  res.sendFile(__dirname + '/views/index.html' )
-})
+})                                         //#2+3//
+
 
 
 
