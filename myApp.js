c3c9481f497578e88417356a74a12c21cc0d067e
matan solom <matan.s.dev@gmail.com>
2023-12-14 19:55:32 +0200

    4-8

--- myApp.js
+++ myApp.js
@@ -2,22 +2,43 @@ let express = require('express');
 let app = express();
 require('dotenv').config();
 
-// console.log("Hello World");  //#1//
+console.log("server restarted");  //#1//
 
-app.use('/public',express.static(__dirname + '/public'))                   //#4//
+app.use('/public',express.static(__dirname + '/public'))  //#4//
+
+//#7
+// app.use((req, res,next)=>{
+//     console.log(req.method + " " +req.path + " "+"-"+" " + req.ip)
+//     next();
+// })
 
 app.get("/",function(req, res) {
  res.sendFile(__dirname + '/views/index.html' )
+ 
 })                                         //#2+3//
 
-// 5
+// #5
 app.get("/json",(req, res)=>{
+    //#6
  if(process.env.MESSAGE_STYLE=="uppercase"){
   res.json({"message":"HELLO JSON"})
  }else{
   res.json({"message":"Hello json"})
  }
 })
+
+const getCurrentTime=()=>{
+    return new Date().toString();
+}
+app.get("/now",(req, res, next)=>{
+    req.time=getCurrentTime();
+    next();
+},(req, res)=>{
+    res.json({"time":req.time})
+})
+
+
+
 
 
 
